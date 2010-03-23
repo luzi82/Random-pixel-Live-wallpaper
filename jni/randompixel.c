fb5915b215b429bd3bd45e9772c0cc11fa4a60c3
luzi82 <luzi82@luzi82-desktop.(none)>
2010-03-23 23:46:19 +0800

    remove comment, format code in C

--- jni/randompixel.c
+++ jni/randompixel.c
@@ -13,21 +13,16 @@ JNIEXPORT void JNICALL Java_com_luzi82_randomwallpaper_LiveWallpaper_genRandom(J
 		jclass cls, jbyteArray out) {
 	static jint*ptr,*buf2,*bufEnd;
 	static jsize len;
-//	static jboolean isCopy;
 	buf2=(jint*)((*env)->GetByteArrayElements(env,out,NULL));
 	if(buf2) {
 		len=(*env)->GetArrayLength(env,out);
-//		if(isCopy){
-//			memset(buf2,0xff,len);
-//		}else{
-			ptr=buf2;
-			bufEnd=buf2+(len>>2);
-			while(ptr!=bufEnd) {
-				seed=(seed*multiplier+0xbLL)&((1LL<<48)-1);
-				*ptr=((int)(seed>>(48-32)))|0xff000000;
-				++ptr;
-			}
-//		}
+		ptr=buf2;
+		bufEnd=buf2+(len>>2);
+		while(ptr!=bufEnd) {
+			seed=(seed*multiplier+0xbLL)&((1LL<<48)-1);
+			*ptr=((int)(seed>>(48-32)))|0xff000000;
+			++ptr;
+		}
 		(*env)->ReleaseByteArrayElements(env,out,(jbyte*)buf2,0);
 	}
 }
