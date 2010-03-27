b8fe418c1de0a6999ed790c0b4857aa9d973435b
luzi82 <luzi82@luzi82-desktop.(none)>
2010-03-28 02:48:11 +0800

    setting completed

--- src/com/luzi82/randomwallpaper/LiveWallpaper.java
+++ src/com/luzi82/randomwallpaper/LiveWallpaper.java
@@ -1,11 +1,14 @@
 package com.luzi82.randomwallpaper;
 
 import java.nio.ByteBuffer;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.service.wallpaper.WallpaperService;
@@ -13,7 +16,8 @@ import android.view.SurfaceHolder;
 
 public class LiveWallpaper extends WallpaperService {
 
-	// private static final String LOG_TAG = "LiveWallpaper";
+	public static final String LOG_TAG = "LiveWallpaper";
+	public static final String PREFERENCE_NAME = "PREF";
 
 	@Override
 	public void onDestroy() {
@@ -29,6 +33,7 @@ public class LiveWallpaper extends WallpaperService {
 
 	static final Matrix mi = new Matrix();
 	static final Paint paint = new Paint();
+	static final Random random = new Random();
 
 	static int oldWidth = -1;
 	static int oldHeight = -1;
@@ -36,6 +41,11 @@ public class LiveWallpaper extends WallpaperService {
 	static byte[] byteAry;
 	static ByteBuffer byteBuffer;
 	static Bitmap bitmap;
+
+	static boolean colorRandom = false;
+	static int color = 0;
+	static boolean doRandom = false;
+	static boolean doColor = false;
 
 	static void clean() {
 		// Log.d(LOG_TAG, "static synchronized void clean()");
@@ -68,26 +78,45 @@ public class LiveWallpaper extends WallpaperService {
 				oldHeight = nowHeight;
 				oldWidth = nowWidth;
 			}
-			if (byteAry == null || byteAry.length != s) {
-				byteAry = new byte[s];
-				byteBuffer = ByteBuffer.wrap(byteAry);
-				// setSize(s);
-				// size = s;
+			if (doRandom) {
+				if (byteAry == null || byteAry.length != s) {
+					byteAry = new byte[s];
+					byteBuffer = ByteBuffer.wrap(byteAry);
+					// setSize(s);
+					// size = s;
+				}
+				genRandom(byteAry);
+				bitmap.copyPixelsFromBuffer(byteBuffer);
 			}
-			genRandom(byteAry);
-			bitmap.copyPixelsFromBuffer(byteBuffer);
 			c.drawBitmap(bitmap, mi, paint);
+			if (doColor) {
+				if (colorRandom) {
+					color = random.nextInt();
+				}
+				c.drawColor(color);
+			}
 		}
 		// Log.d(LOG_TAG, "static void drawCanvas(Canvas c) end");
 	}
 
-	class LiveWallpaperEngine extends Engine {
+	class LiveWallpaperEngine extends Engine implements
+			SharedPreferences.OnSharedPreferenceChangeListener {
 
+		SharedPreferences sp;
 		Timer timer;
+
+		@Override
+		public void onCreate(SurfaceHolder surfaceHolder) {
+			super.onCreate(surfaceHolder);
+			sp = getSharedPreferences(PREFERENCE_NAME, 0);
+		}
 
 		@Override
 		public void onDestroy() {
 			synchronized (mi) {
+				if (sp == null)
+					return;
+				sp.unregisterOnSharedPreferenceChangeListener(this);
 				clearTimer();
 				clean();
 			}
@@ -99,10 +128,15 @@ public class LiveWallpaper extends WallpaperService {
 		public void onVisibilityChanged(boolean visible) {
 			super.onVisibilityChanged(visible);
 			synchronized (mi) {
+				if (sp == null)
+					return;
 				// Log.d(LOG_TAG, "onVisibilityChanged=" + visible);
 				if (visible) {
-					createTimer(this);
+					sp.registerOnSharedPreferenceChangeListener(this);
+					updateColor();
+					createTimer();
 				} else {
+					sp.unregisterOnSharedPreferenceChangeListener(this);
 					clearTimer();
 					clean();
 				}
@@ -133,20 +167,23 @@ public class LiveWallpaper extends WallpaperService {
 			// Log.d(LOG_TAG, "updateCanvas end");
 		}
 
-		void createTimer(final LiveWallpaperEngine engine) {
+		void createTimer() {
 			// Log
 			// .d(LOG_TAG,
 			// "static void createTimer(final LiveWallpaperEngine engine) start");
 			synchronized (mi) {
+				if (sp == null)
+					return;
+				int rateInt = Settings.getRefreshPeroid(sp);
 				if (timer == null) {
 					// Log.d(LOG_TAG, "create timer");
 					timer = new Timer();
 					timer.scheduleAtFixedRate(new TimerTask() {
 						@Override
 						public void run() {
-							engine.updateCanvas();
+							updateCanvas();
 						}
-					}, 100, 100);
+					}, rateInt, rateInt);
 				}
 			}
 			// Log
@@ -160,6 +197,38 @@ public class LiveWallpaper extends WallpaperService {
 					timer.cancel();
 					timer = null;
 				}
+			}
+		}
+
+		void updateColor() {
+			synchronized (mi) {
+				if (sp == null)
+					return;
+				colorRandom = Settings.getColorCheat(sp);
+				if (colorRandom) {
+					doRandom = true;
+					doColor = true;
+				} else {
+					color = Settings.getColor(sp);
+					int alpha = Color.alpha(color);
+					doRandom = alpha != 0xff;
+					doColor = alpha != 0;
+				}
+			}
+		}
+
+		@Override
+		public void onSharedPreferenceChanged(
+				SharedPreferences sharedPreferences, String key) {
+			if (!isVisible())
+				return;
+			if (key.equals(Settings.REFRESH_PEROID_KEY)) {
+				synchronized (mi) {
+					clearTimer();
+					createTimer();
+				}
+			} else if (key.equals(Settings.REFRESH_PEROID_KEY)) {
+				updateColor();
 			}
 		}
 	}
